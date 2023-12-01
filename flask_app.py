b3eb3ee508da956aef59f9cef6fa33e6e4f308e4
akashm-18 <akashmurugan338@gmail.com>
2023-12-01 23:41:17 +0530

    notes gets some words

--- flask_app.py
+++ flask_app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, url_for, request, flash, redirect
+from sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
-
 app.config["SECRET_KEY"] = "cf11bfa1c7ea958a4e36ea87451dedd0"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
+db = SQLAlchemy(app)
 
 posts = [
     {
